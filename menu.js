2deca80efa6a080b25c6811f81fad4cc88554700
Quinn <quinn@big-brother.ga>
2018-12-27 10:27:36 -0800

    Finish Menu Updates

--- menu.js
+++ menu.js
@@ -2,5 +2,6 @@ document.getElementById("navMenu").innerHTML =
 '<ul>'+
   '<li><a href="/">Home</a></li>'+
   '<li><a href="/staff">Staff</a></li>'+
-  '<li><a href="/commands" class="active">Commands</a></li>'+
+  '<li><a href="/commands">Commands</a></li>'+
+  '<li><a href="/ccguide">Guide</a></li>'+
 '</ul>'+
