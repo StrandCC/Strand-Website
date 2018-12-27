bfe336e1635d0ebb68f55b42ec80b98abbef295e
Quinn <quinn@big-brother.ga>
2018-12-27 10:19:41 -0800

    Syncing the menus (among other things)

---
+++ menu.js
@@ -0,0 +1,6 @@
+document.getElementById("navMenu").innerHTML =
+'<ul>'+
+  '<li><a href="/">Home</a></li>'+
+  '<li><a href="/staff">Staff</a></li>'+
+  '<li><a href="/commands" class="active">Commands</a></li>'+
+'</ul>'+
