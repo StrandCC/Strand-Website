c11870a83ec24c892ab72536a107df68d6319a77
Quinn <quinn@big-brother.ga>
2018-12-31 17:02:29 -0800

    Change staff to team in menu

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 document.getElementById("navMenu").innerHTML =
 '<ul>'+
   '<li><a href="/">Home</a></li>'+
-  '<li><a href="/staff">Staff</a></li>'+
+  '<li><a href="/staff">Team</a></li>'+
   '<li><a href="/commands">Commands</a></li>'+
   '<li><a href="/ccguide">Guide</a></li>'+
 '</ul>';
