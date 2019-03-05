f3cb7b03002f6199f467cd0c16199c6c4499ac20
Quinn <quinntdip@gmail.com>
2019-03-04 19:28:28 -0600

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,6 +2,6 @@ document.getElementById("navMenu").innerHTML =
 '<ul>'+
   '<li><a href="/">Home</a></li>'+
   '<li><a href="/team">Team</a></li>'+
-  '<li><a href="/commands">Commands</a></li>'+
+  '<li><a href="/commands">Command Directory</a></li>'+
   '<li><a href="/ccguide">Guide</a></li>'+
 '</ul>';
