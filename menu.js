72a63fb97a770b58acc077e122eae3319b0bd7e3
Quinn <quinntdip@gmail.com>
2019-03-04 19:41:22 -0600

    [index.js] changing titles because space

--- menu.js
+++ menu.js
@@ -3,6 +3,6 @@ document.getElementById("navMenu").innerHTML =
   '<li><a href="/">Home</a></li>'+
   '<li><a href="/team">Team</a></li>'+
   '<li><a href="/ccguide">Guide</a></li>'+
-  '<li><a href="/commands">Command Directory</a></li>'+
-  '<li><a href="/modules">Module Directory</a></li>'+
+  '<li><a href="/commands">Command Index</a></li>'+
+  '<li><a href="/modules">Module Index</a></li>'+
 '</ul>';
